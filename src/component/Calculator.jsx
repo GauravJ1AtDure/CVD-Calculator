39b65553ffbf667358072f57231a571e4c517583
Gauravj12 <gauravjadhav1205@gmail.com>
2023-06-17 21:29:03 +0530

    working on getting calculat arr

--- src/component/Calculator.jsx
+++ src/component/Calculator.jsx
@@ -1,22 +1,42 @@
 import React , {useState, useEffect} from 'react'
+import regionlists from './RegionData';
 
 function Calculator() {
     const [countryArr, setCountryArr] = useState([]);
     const [labBasedArr, setLabBasedArr]=useState([]);
     const [nonLabBasedArr, setNonLabBasedArr]=useState([]);
-    const [country, setCountry]=useState();
+    const [country, setCountry]=useState('');
+    const [region, setRegion]=useState('')
+    const [gender, setGender]=useState('');
     const [useEffectArr, setUseEffectArr]=useState(1);
+    const [regionOfCountries, setRegionOfCountries]=useState([])
+    const [calArr, setCalArr]=useState([])
+
+
 
     useEffect(() => {
 
-      
+         setRegionOfCountries(regionlists)
          getCountryData()
          getLabBasedData()
          getNonLabBasedData()
-       
-      
-     }
-     , [useEffectArr]);
+         
+     },[useEffectArr]);
+
+     const handleChange = (e,c) => {
+      setUseEffectArr(useEffectArr+1)
+      c=e.target.value
+      setCountry(c)
+      let reg=regionOfCountries.map((val)=>val[c])
+      setRegion(reg)
+      };
+
+
+      const changeGender=(e,g)=>{
+      g=e.target.value
+      setUseEffectArr(useEffectArr+1)
+      setGender(g)
+      }
 
 
      const getCountryData=()=>{
@@ -28,7 +48,7 @@ function Calculator() {
            .catch((err) => {
               console.log(err.message);
            });
-           console.log('countryArr',countryArr);
+           //console.log('countryArr',countryArr);
          }
 
 
@@ -41,7 +61,8 @@ function Calculator() {
                   .catch((err) => {
                      console.log(err.message);
                   });
-                  console.log('labBasedArr',labBasedArr);
+                 // console.log('labBasedArr',labBasedArr);
+                
                 }
 
 
@@ -55,23 +76,23 @@ function Calculator() {
                         .catch((err) => {
                            console.log(err.message);
                         });
-                        console.log('nonLabBasedArr',nonLabBasedArr);
+                        //console.log('nonLabBasedArr',nonLabBasedArr);
                         
                       }
 
-
-
-     const handleChange = (e) => {
-      console.log(e.target.value)
-      setUseEffectArr(useEffectArr+1)
-      };
-
-
-
+/*const getArr=()=>{
+   let newArray = labBasedArr.map((val)=>val[country])
+   console.log('newArray',newArray);
+   setCalArr(newArray)
+   
+}
+*/
+    
       
      return (
         <div className="container">
-            <select className="form-select" aria-label="Default select example" onChange={handleChange}>
+        LAB BASED CVD RISK
+         <select className="form-select" aria-label="Default select example" onChange={handleChange}>
             <option defaultValue>Select Country</option>
                {countryArr.map((elements, index) => {
               return (
@@ -79,7 +100,25 @@ function Calculator() {
               );
            })}
           </select>
-          <p>{country}</p>
+          <input className="form-control" type="number" placeholder="Age (40 to 74 years only)" />
+          <select className="form-select" aria-label="Default select example" onChange={changeGender}>
+          <option defaultValue>Gender</option>
+          <option value="Male">Male</option>
+          <option value="Female">Female</option>
+          </select>
+          <select className="form-select" aria-label="Default select example">
+          <option defaultValue>Smoking</option>
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+          </select>
+          <select className="form-select" aria-label="Default select example">
+          <option defaultValue>Diabetes</option>
+          <option value="yes">Yes</option>
+          <option value="no">No</option>
+          </select>
+          <input className="form-control" type="number" placeholder="Systolic BP" />
+          <input className="form-control" type="number" placeholder="Total cholestrol (in mmol)" />
+          <p>{region}{gender}</p>
         </div>
         );
   
