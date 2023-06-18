c1da4283b1af060e5aa6073bb189b04c5eea97b4
new name <gauravjadhav1205@gmail.com>
2023-06-18 13:20:41 +0530

    working on calculation

---
+++ src/component/CalculateTable.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function CalculateTable(props) {
+
+
+        let { title, region, gender, age_chd, age_stroke, chol_chd_1, chol_chd_2, chol_stroke_1, chol_stroke_2, sys_chd_1, sys_chd_2, sys_stroke_1, sys_stroke_2, diab_chd_1, diab_chd_2, diab_stroke_1, diab_stroke_2, smok_chd_1, smok_chd_2, smok_stroke_1, smok_stroke_2, uncalibrated_chd, calibrated_chd1, calibrated_chd2, uncalibrated_stroke, calibrated_stroke1, calibrated_stroke2 } = props;
+
+        
+return(
+    <div>
+      <p></p>
+    </div>
+  )
+}
+
+export default CalculateTable
--- src/component/Calculator.jsx
+++ src/component/Calculator.jsx
@@ -1,5 +1,6 @@
 import React , {useState, useEffect} from 'react'
 import regionlists from './RegionData';
+import CalculateTable from './CalculateTable';
 
 function Calculator() {
    const [regionOfCountries, setRegionOfCountries]=useState(regionlists)
@@ -62,7 +63,7 @@ function Calculator() {
                   .catch((err) => {
                      console.log(err.message);
                   });
-                  console.log('labBasedArr',labBasedArr);
+                  //console.log('labBasedArr',labBasedArr);
                 
                 }
 
@@ -85,9 +86,12 @@ const getArr=()=>{
    let newArray = labBasedArr.filter((val) => {return(
             val.region===region && val.gender===gender)
    })
-   console.log('region',region)
-   console.log('gender',gender)
-   console.log('newArray',newArray)
+   setCalArr(newArray)
+   
+   
+}
+const getData=()=>{
+   console.log(calArr)
 }
 
     
@@ -109,7 +113,7 @@ const getArr=()=>{
           <option value="Male">Male</option>
           <option value="Female">Female</option>
           </select>
-          <select className="form-select" aria-label="Default select example">
+          <select className="form-select" aria-label="Default select example" onChange={getArr}>
           <option defaultValue>Smoking</option>
           <option value="yes">Yes</option>
           <option value="no">No</option>
@@ -121,8 +125,14 @@ const getArr=()=>{
           </select>
           <input className="form-control" type="number" placeholder="Systolic BP" />
           <input className="form-control" type="number" placeholder="Total cholestrol (in mmol)" />
-          <p>{region}{gender}</p>
-          <button onClick={getArr}>getData</button>
+          <button onClick={getData}>getData</button>
+          {/* <CalculateTable title={calArr[0].title===undefined ? 'data not loaded yet' : calArr[0].title} region={calArr[0].region} gender={calArr[0].gender}
+         age_chd={calArr[0].age_chd} age_stroke={calArr[0].age_stroke} chol_chd_1={calArr[0].chol_chd_1}
+         chol_chd_2={calArr[0].chol_chd_2} chol_stroke_1={calArr[0].chol_stroke_1} chol_stroke_2={calArr[0].chol_stroke_2}
+         sys_chd_1={calArr[0].sys_chd_1} sys_chd_2={calArr[0].sys_chd_2} sys_stroke_1={calArr[0].sys_stroke_1}
+         sys_stroke_2={calArr[0].sys_stroke_2} diab_chd_1={calArr[0].diab_chd_1} diab_chd_2={calArr[0].diab_chd_2}
+         diab_stroke_1={calArr[0].diab_stroke_1} diab_stroke_2={calArr[0].diab_stroke_2} smok_chd_1={calArr[0].smok_chd_1} smok_chd_2={calArr[0].smok_chd_2} smok_stroke_1={calArr[0].smok_stroke_1} smok_stroke_2={calArr[0].smok_stroke_2}
+         uncalibrated_chd={calArr[0].uncalibrated_chd} calibrated_chd1={calArr[0].calibrated_chd1} calibrated_chd2={calArr[0].calibrated_chd2} uncalibrated_stroke={calArr[0].uncalibrated_stroke} calibrated_stroke1={calArr[0].calibrated_stroke1} calibrated_stroke2={calArr[0].calibrated_stroke2}/> */}
         </div>
         );
   
