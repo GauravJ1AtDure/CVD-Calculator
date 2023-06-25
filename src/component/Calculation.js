1df22686410efccc6c4cfb589e2a9342c9aa5737
new name <gauravjadhav1205@gmail.com>
2023-06-25 13:47:00 +0530

    lab based calculation complete

--- src/component/Calculation.js
+++ src/component/Calculation.js
@@ -2,62 +2,73 @@ import React, {useState} from 'react'
 
 const Calculation=(props)=> {
 
-  const [chdValue, setChdValue]=useState();
-  const [strokeValue, setstrokeValue]=useState();
+  const [cvdRiskVal, setCvdRiskVal]=useState();
+  
 
   let { calData, country, age, usrGender, smoking, diabetes, bp, cholestrol } = props;
+
+  let usrAge=parseInt(age)
+  let usrBp=parseInt(bp)
+  let usrChol=parseFloat(cholestrol)
   
- 
+  
+    let rfAge, rfTotChol, rfCholestrol, rfSysBp, rfDibetes, rfSmoking;
 
-    let rfAge, rfTotChol, rfRoundChol, rfSysBp, rfDibetes, rfSmoking;
-
-    rfAge=age-60
-    rfTotChol=cholestrol-6
-    rfRoundChol=rfTotChol.toFixed(1)
-    rfSysBp=bp-120
+    rfAge=usrAge-60
+    rfTotChol=usrChol-6
+    rfCholestrol=parseFloat(rfTotChol)
+    rfSysBp=usrBp-120
     rfDibetes=diabetes==='yes'? 1 : 0
     rfSmoking=smoking==='yes'? 1 : 0 
 
 
-    const getArr1=(chdAge, chdChol, chdBp, chdDibetes, chdSmoking, chdTot, strokeAge, strokeChol, strokeBp, strokeDibetes, strokeSmoking, strokeTot, uncalibratedProbChd, calibratedProbChd)=>{
+    const getArr1=(chdAge, chdChol, chdBp, chdDibetes, chdSmoking, chdTot, strokeAge, strokeChol, strokeBp, strokeDibetes, strokeSmoking, strokeTot, uncalibratedProbChd, calibratedProbChd, uncalibratedProbStroke, calibratedProbStroke, calibratedProbCvd, finalCvdRiskPrediction)=>{
 
       let { title, region, gender, age_chd, age_stroke, chol_chd_1, chol_chd_2, chol_stroke_1, chol_stroke_2, sys_chd_1, sys_chd_2, sys_stroke_1, sys_stroke_2, diab_chd_1, diab_chd_2, diab_stroke_1, diab_stroke_2, smok_chd_1, smok_chd_2, smok_stroke_1, smok_stroke_2, uncalibrated_chd, calibrated_chd1, calibrated_chd2, uncalibrated_stroke, calibrated_stroke1, calibrated_stroke2 } = calData;
 
-      
 
         chdAge=age_chd*rfAge
-        chdChol=chol_chd_1*rfRoundChol-chol_chd_2*rfAge*rfRoundChol
+        chdChol=chol_chd_1*rfCholestrol-chol_chd_2*rfAge*rfCholestrol
         chdBp=sys_chd_1*rfSysBp-sys_chd_2*rfAge*rfSysBp
         chdDibetes=diab_chd_1*rfDibetes-diab_chd_2*rfAge*rfDibetes
         chdSmoking=smok_chd_1*rfSmoking-smok_chd_2*rfAge*rfSmoking
         chdTot=chdAge+chdChol+chdBp+chdDibetes+chdSmoking
         
-        setChdValue(chdTot)
-
 
         strokeAge=age_stroke*rfAge
-        strokeChol=chol_stroke_1*rfRoundChol+chol_stroke_2*rfAge*rfRoundChol
+        strokeChol=chol_stroke_1*rfCholestrol+chol_stroke_2*rfAge*rfCholestrol
         strokeBp=sys_stroke_1*rfSysBp-sys_stroke_2*rfAge*rfSysBp
         strokeDibetes=diab_stroke_1*rfDibetes-diab_stroke_2*rfAge*rfDibetes
         strokeSmoking=smok_stroke_1*rfSmoking-smok_stroke_2*rfAge*rfSmoking
         strokeTot=strokeAge+strokeChol+strokeBp+strokeDibetes+strokeSmoking
 
-        setstrokeValue(strokeTot)
         
         uncalibratedProbChd = (1-uncalibrated_chd ** Math.exp(chdTot))
-        let a = calibrated_chd1+calibrated_chd2*Math.log(-Math.log(1-uncalibratedProbChd))
-        console.log(a)
-
-
         
-       
+        let calibrated_chd1Int=parseFloat(calibrated_chd1)
+        let calibrated_chd2Int=parseFloat(calibrated_chd2)
+        
+        calibratedProbChd = 1-Math.exp(-Math.exp(calibrated_chd1Int+calibrated_chd2Int*Math.log(-Math.log(1-uncalibratedProbChd))))
+
+        uncalibratedProbStroke = 1-uncalibrated_stroke ** Math.exp(strokeTot)
+
+        let calibrated_stroke1Int=parseFloat(calibrated_stroke1)
+        let calibrated_stroke2Int=parseFloat(calibrated_stroke2)
+        
+        calibratedProbStroke = 1-Math.exp(-Math.exp(calibrated_stroke1Int+calibrated_stroke2Int*Math.log(-Math.log(1-uncalibratedProbStroke))))
+
+        calibratedProbCvd = 1-(1-calibratedProbChd)*(1-calibratedProbStroke)
+
+        finalCvdRiskPrediction=Math.round(calibratedProbCvd*100)
+        setCvdRiskVal(finalCvdRiskPrediction) 
+        console.log(cvdRiskVal)
     }
 
 
   return (
     <div>
-      <button onClick={getArr1}>Get Array</button>
-      <p></p>
+      <button onClick={getArr1}>Calculate CVD Risk</button>
+      <p>{cvdRiskVal}</p>
     </div>
   )
 }
--- src/component/Calculator.jsx
+++ src/component/Calculator.jsx
@@ -1,6 +1,6 @@
 import React , {useState, useEffect} from 'react'
 import regionlists from './RegionData';
-import CalculateTable from './CalculateTable';
+import Calculation from './Calculation';
 
 function Calculator() {
    const [regionOfCountries, setRegionOfCountries]=useState(regionlists)
@@ -10,6 +10,11 @@ function Calculator() {
     const [country, setCountry]=useState('');
     const [region, setRegion]=useState()
     const [gender, setGender]=useState();
+    const [age, setAge]=useState();
+    const [smoke, setSmoke]=useState();
+    const [bp, setBp]=useState();
+    const [cholestrol, setCholestrol]=useState();
+    const [diabetes, setDiabetes]=useState();
     const [useEffectArr, setUseEffectArr]=useState(1);
     const [calArr, setCalArr]=useState([])
 
@@ -40,6 +45,12 @@ function Calculator() {
       setGender(g)
       }
 
+      const getAge=(e)=>{
+         setAge(e.target.value)
+      }
+
+
+
 
      const getCountryData=()=>{
      fetch('https://ncd-pen.duredemos.com/json/countries?_format=json')
@@ -50,7 +61,6 @@ function Calculator() {
            .catch((err) => {
               console.log(err.message);
            });
-           //console.log('countryArr',countryArr);
          }
 
 
@@ -63,13 +73,12 @@ function Calculator() {
                   .catch((err) => {
                      console.log(err.message);
                   });
-                  //console.log('labBasedArr',labBasedArr);
                 
                 }
 
 
          const getNonLabBasedData=()=>{
-               fetch('https://ncd-pen.duredemos.com/json/lab-based?_format=json')
+               fetch('https://ncd-pen.duredemos.com/json/nonlab-based?_format=json')
                         .then((response) => response.json())
                         .then((nonLabData) => {
                            setNonLabBasedArr(nonLabData);
@@ -78,21 +87,29 @@ function Calculator() {
                         .catch((err) => {
                            console.log(err.message);
                         });
-                        //console.log('nonLabBasedArr',nonLabBasedArr);
                         
                       }
 
-const getArr=()=>{
+const getArr=(e)=>{
    let newArray = labBasedArr.filter((val) => {return(
             val.region===region && val.gender===gender)
    })
-   setCalArr(newArray)
-   
+   setSmoke(e.target.value)
+   setCalArr(...newArray)
    
 }
-const getData=()=>{
-   console.log(calArr)
+
+const getDiabetes=(e)=>{
+   setDiabetes(e.target.value)
 }
+const getBp=(e)=>{
+   setBp(e.target.value)
+}
+const getCholestrol=(e)=>{
+   setCholestrol(e.target.value)
+}
+
+
 
     
       
@@ -107,7 +124,7 @@ const getData=()=>{
               );
            })}
           </select>
-          <input className="form-control" type="number" placeholder="Age (40 to 74 years only)" />
+          <input className="form-control" type="number" placeholder="Age (40 to 74 years only)" onChange={getAge} />
           <select className="form-select" aria-label="Default select example" onChange={changeGender}>
           <option defaultValue>Gender</option>
           <option value="Male">Male</option>
@@ -118,21 +135,14 @@ const getData=()=>{
           <option value="yes">Yes</option>
           <option value="no">No</option>
           </select>
-          <select className="form-select" aria-label="Default select example">
+          <select className="form-select" aria-label="Default select example" onChange={getDiabetes}>
           <option defaultValue>Diabetes</option>
           <option value="yes">Yes</option>
           <option value="no">No</option>
           </select>
-          <input className="form-control" type="number" placeholder="Systolic BP" />
-          <input className="form-control" type="number" placeholder="Total cholestrol (in mmol)" />
-          <button onClick={getData}>getData</button>
-          {/* <CalculateTable title={calArr[0].title===undefined ? 'data not loaded yet' : calArr[0].title} region={calArr[0].region} gender={calArr[0].gender}
-         age_chd={calArr[0].age_chd} age_stroke={calArr[0].age_stroke} chol_chd_1={calArr[0].chol_chd_1}
-         chol_chd_2={calArr[0].chol_chd_2} chol_stroke_1={calArr[0].chol_stroke_1} chol_stroke_2={calArr[0].chol_stroke_2}
-         sys_chd_1={calArr[0].sys_chd_1} sys_chd_2={calArr[0].sys_chd_2} sys_stroke_1={calArr[0].sys_stroke_1}
-         sys_stroke_2={calArr[0].sys_stroke_2} diab_chd_1={calArr[0].diab_chd_1} diab_chd_2={calArr[0].diab_chd_2}
-         diab_stroke_1={calArr[0].diab_stroke_1} diab_stroke_2={calArr[0].diab_stroke_2} smok_chd_1={calArr[0].smok_chd_1} smok_chd_2={calArr[0].smok_chd_2} smok_stroke_1={calArr[0].smok_stroke_1} smok_stroke_2={calArr[0].smok_stroke_2}
-         uncalibrated_chd={calArr[0].uncalibrated_chd} calibrated_chd1={calArr[0].calibrated_chd1} calibrated_chd2={calArr[0].calibrated_chd2} uncalibrated_stroke={calArr[0].uncalibrated_stroke} calibrated_stroke1={calArr[0].calibrated_stroke1} calibrated_stroke2={calArr[0].calibrated_stroke2}/> */}
+          <input className="form-control" type="number" placeholder="Systolic BP" onChange={getBp}/>
+          <input className="form-control" type="number" placeholder="Total cholestrol (in mmol)" onChange={getCholestrol} />
+          <Calculation calData={calArr} country={country} age={age} usrGender={gender} smoking={smoke} diabetes={diabetes} bp={bp} cholestrol={cholestrol}/>
         </div>
         );
   
